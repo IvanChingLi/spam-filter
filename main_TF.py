c8b07c14275b3fc2686989f5f46851ce716ab8a4
IvanChingLi <33820182+IvanChingLi@users.noreply.github.com>
2017-12-02 14:40:08 -0800

    comment to test GitHub desktop

--- main_TF.py
+++ main_TF.py
@@ -10,6 +10,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 
 ##Would changing this comment on github.com change what is locally saved?
+##Okay but what if I add another comment from github.com???
 
 def preprocess(readfilename, writefilename):
     print("Preprocessing...")
